3a77f7328fcc122852ef60387e690c5c88fdb07b
MadladV <cgkustosz@gmail.com>
2025-11-01 13:44:08 +0100

    Added default value for AxialDeadzone

--- scripts/ControllerSettings.cs
+++ scripts/ControllerSettings.cs
@@ -3,7 +3,7 @@ namespace GodotGyro;
 public class ControllerSettings
 {
     public float Deadzone { get; set; } = 0.1f;
-    public float AxialDeadzone { get; set; }
+    public float AxialDeadzone { get; set; } = 0f;
     public float TurnRate { get; set; } = 360f;
     public float VerticalSensitivity { get; set; } = 0.67f;
     public float Curve { get; set; } = 3f;
