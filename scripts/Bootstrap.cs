2be705bc6d379fa011f020fc0e91efa2dce7a4aa
MadladV <cgkustosz@gmail.com>
2025-11-01 19:33:15 +0100

    Updated the autoload

--- scripts/Bootstrap.cs
+++ scripts/Bootstrap.cs
@@ -7,10 +7,12 @@ public partial class Bootstrap : Node
 {
     public override void _Ready()
     {
-        Singleton<GyroProcessor>.Instance = new GyroProcessor();
-        Singleton<FlickStick>.Instance = new FlickStick();
-        Singleton<GyroSettings>.Instance = new GyroSettings();
         Singleton<ControllerSettings>.Instance = new ControllerSettings();
+        Singleton<FlickStick>.Instance = new FlickStick();
+        Singleton<GodotGyro>.Instance = new GodotGyro();
+        Singleton<GyroInput>.Instance = new GyroInput();
+        Singleton<GyroProcessor>.Instance = new GyroProcessor();
+        Singleton<GyroSettings>.Instance = new GyroSettings();
     }
 }
 
