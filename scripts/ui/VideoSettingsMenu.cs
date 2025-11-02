ce0f004e1decdc01d5da456fc08b8585fb668f68
MadladV <cgkustosz@gmail.com>
2025-11-02 14:12:50 +0100

    Added display settings

--- scripts/ui/VideoSettingsMenu.cs
+++ scripts/ui/VideoSettingsMenu.cs
@@ -3,7 +3,34 @@ using Godot;
 namespace GodotGyro;
 public partial class VideoSettingsMenu : PanelContainer
 {
-    // TODO: Window size
-    // TODO: Windowed / Borderless switch
-    // TODO: Vsync
+    [Export] private SettingSlider resolutionScale;
+    [Export] private OptionButton windowResolution, displayMode;
+    [Export] private Button applyButton;
+
+    private Vector2I[] resolutions = [new (1280, 720), new (1920, 1080), new (2560, 1440), new (3840, 2160)];
+    private float newResolutionScale;
+    private Vector2I newResolution;
+    private DisplayServer.WindowMode newDisplayMode;
+    
+    public override void _Ready()
+    {
+        resolutionScale.OnValueChanged += (_, value) =>
+        {
+            newResolutionScale = value;
+        };
+        windowResolution.ItemSelected += (value) =>
+        {
+            newResolution = resolutions[value];
+        };
+        displayMode.ItemSelected += (value) =>
+        {
+            newDisplayMode = (DisplayServer.WindowMode)value;
+        };
+        applyButton.Pressed += () =>
+        {
+            DisplayServer.WindowSetSize(newResolution);
+            DisplayServer.WindowSetMode(newDisplayMode);
+            GetViewport().Scaling3DScale = newResolutionScale;
+        };
+    }
 }
