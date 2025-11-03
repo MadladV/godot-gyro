2416095d01382976bdeffcb47be84937f95ec0de
MadladV <cgkustosz@gmail.com>
2025-11-03 15:42:21 +0100

    Fixed a script error in settings class

--- scripts/ui/VideoSettingsMenu.cs
+++ scripts/ui/VideoSettingsMenu.cs
@@ -7,7 +7,7 @@ public partial class VideoSettingsMenu : PanelContainer
     [Export] private OptionButton windowResolution, displayMode;
     [Export] private Button applyButton;
 
-    private Vector2I[] resolutions = [new (1280, 720), new (1920, 1080), new (2560, 1440), new (3840, 2160)];
+    private readonly Vector2I[] resolutions = [new (1280, 720), new (1920, 1080), new (2560, 1440), new (3840, 2160)];
     private float newResolutionScale = 1.0f;
     private Vector2I newResolution = new (1280, 720);
     private DisplayServer.WindowMode newDisplayMode =  DisplayServer.WindowMode.Windowed;
@@ -31,6 +31,7 @@ public partial class VideoSettingsMenu : PanelContainer
                     break;
                 case 1:
                     newDisplayMode = DisplayServer.WindowMode.Fullscreen;
+                    break;
             }
         };
         applyButton.Pressed += () =>
