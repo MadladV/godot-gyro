b9bb6dab58e270d6190f9b56449787ebcef16245
MadladV <cgkustosz@gmail.com>
2025-11-16 17:24:34 +0100

    Improved UI scaling with resolution

--- scripts/ui/VideoSettingsMenu.cs
+++ scripts/ui/VideoSettingsMenu.cs
@@ -6,11 +6,13 @@ public partial class VideoSettingsMenu : PanelContainer
     [Export] private SettingSlider resolutionScale;
     [Export] private OptionButton windowResolution, displayMode;
     [Export] private Button applyButton;
+    [Export] private CanvasLayer buttonPromptCanvas;
 
     private readonly Vector2I[] resolutions = [new (1280, 720), new (1920, 1080), new (2560, 1440), new (3840, 2160)];
     private float newResolutionScale = 1.0f;
     private Vector2I newResolution = new (1280, 720);
     private DisplayServer.WindowMode newDisplayMode =  DisplayServer.WindowMode.Windowed;
+    private Vector2I defaultResolution = new (2560, 1440);
     
     public override void _Ready()
     {
@@ -39,6 +41,17 @@ public partial class VideoSettingsMenu : PanelContainer
             DisplayServer.WindowSetSize(newResolution);
             DisplayServer.WindowSetMode(newDisplayMode);
             GetViewport().Scaling3DScale = newResolutionScale;
+        };
+
+        GetViewport().SizeChanged += () =>
+        {
+            Vector2 scale = GetViewportRect().Size / defaultResolution;
+            GD.Print(scale);
+            buttonPromptCanvas.Transform = new Transform2D(
+                new Vector2(scale.X, 0f), 
+                new Vector2(0f, scale.X), 
+                new Vector2(GetViewportRect().Size.X * (1f - scale.X), GetViewportRect().Size.Y * (1f - scale.Y))
+            );
         };
     }
 }
